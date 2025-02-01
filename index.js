367fecd15ce55ecf8c90ec655cda3b3d42bb410c
Slava <v.beloglazov@rambler-co.ru>
2025-02-01 22:39:59 +0300

    add vite, manual animation

--- index.js
+++ index.js
@@ -1,17 +1,145 @@
 const tipEl = document.getElementById('tip');
 const catEl = document.getElementById('cat');
+const frameContainerEl = document.getElementById('frame-container');
+
+const fps = 60;
+const AnimationState = {
+  Idle: 'idle',
+  Play: 'play',
+};
+const frames = [
+  'frame23.png',
+  'frame24.png',
+  'frame25.png',
+  'frame26.png',
+  'frame27.png',
+  'frame28.png',
+  'frame29.png',
+  'frame30.png',
+  'frame31.png',
+  'frame32.png',
+  'frame33.png',
+  'frame34.png',
+  'frame35.png',
+  'frame36.png',
+  'frame37.png',
+  'frame38.png',
+  'frame39.png',
+  'frame40.png',
+  'frame41.png',
+  'frame42.png',
+  'frame43.png',
+  'frame44.png',
+  'frame45.png',
+  'frame46.png',
+  'frame47.png',
+  'frame48.png',
+  'frame49.png',
+  'frame50.png',
+  'frame51.png',
+  'frame52.png',
+  'frame53.png',
+  'frame54.png',
+  'frame55.png',
+  'frame56.png',
+  'frame57.png',
+  'frame58.png',
+  'frame59.png',
+  'frame60.png',
+  'frame61.png',
+  'frame62.png',
+  'frame63.png',
+  'frame64.png',
+  'frame65.png',
+  'frame66.png',
+  'frame67.png',
+  'frame68.png',
+  'frame69.png',
+  'frame70.png',
+  'frame71.png',
+  'frame72.png',
+  'frame73.png',
+  'frame74.png',
+  'frame75.png',
+  'frame76.png',
+  'frame77.png',
+  'frame78.png',
+  'frame79.png',
+  'frame80.png',
+  'frame81.png',
+  'frame82.png',
+  'frame83.png',
+  'frame84.png',
+  'frame85.png',
+  'frame86.png',
+  'frame87.png',
+  'frame88.png',
+  'frame89.png',
+  'frame90.png',
+  'frame91.png',
+  'frame92.png',
+  'frame93.png',
+  'frame94.png',
+  'frame95.png',
+  'frame96.png',
+  'frame97.png',
+  'frame98.png',
+  'frame99.png',
+  'frame100.png',
+  'frame101.png',
+  'frame102.png',
+  'frame103.png',
+  'frame104.png',
+  'frame105.png',
+  'frame106.png',
+  'frame107.png',
+  'frame108.png',
+  'frame109.png',
+  'frame110.png',
+  'frame111.png',
+  'frame112.png',
+  'frame113.png',
+  'frame114.png',
+  'frame115.png',
+  'frame116.png',
+  'frame1.png',
+  'frame2.png',
+  'frame3.png',
+  'frame4.png',
+  'frame5.png',
+  'frame6.png',
+  'frame7.png',
+  'frame8.png',
+  'frame9.png',
+  'frame10.png',
+  'frame11.png',
+  'frame12.png',
+  'frame13.png',
+  'frame14.png',
+  'frame15.png',
+  'frame16.png',
+  'frame17.png',
+  'frame18.png',
+  'frame19.png',
+  'frame20.png',
+  'frame21.png',
+  'frame22.png',
+];
 
 let firstPlay = true;
-let timeoutId;
+let animationState = AnimationState.Idle;
+let intervalId;
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 function letGo() {
-  if (!catEl) {
+  if (!catEl || animationState === AnimationState.Play) {
     return;
   }
 
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
+  animationState = AnimationState.Play;
 
   if (firstPlay) {
     firstPlay = false;
@@ -19,12 +147,19 @@ function letGo() {
     tipEl.classList.add('hidden');
   }
 
-  catEl.src = `./assets/animated-cat.png`;
+  const animate = async () => {
+    for await (const frame of frames) {
+      requestAnimationFrame(() => {
+        catEl.src = `/cat-frames/${frame}`;
+      });
 
-  timeoutId = setTimeout(() => {
-    catEl.src = `./assets/static-cat.png`;
-    clearTimeout(timeoutId);
-  }, 1500);
+      await sleep(1000 / fps);
+    }
+
+    animationState = AnimationState.Idle;
+  };
+
+  animate();
 }
 
 function handleSpaceKeyDown(event) {
