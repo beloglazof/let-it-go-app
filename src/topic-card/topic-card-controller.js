be6c9ad873ab0793bfa288d6b38a07171200a708
Slava <slavathedeveloper@gmail.com>
2025-02-16 12:45:30 +0300

    add clear input button

--- src/topic-card/topic-card-controller.js
+++ src/topic-card/topic-card-controller.js
@@ -1,16 +1,22 @@
 const placeholderId = 'topic-card-placeholder';
 const topicCardBodyEl = document.getElementById('topic-card-body');
+const clearButtonEl = document.getElementById('clear-button');
 
 let placeholderEl = document.getElementById(placeholderId);
 
 function createCardPlaceholder() {
   const el = document.createElement('span');
 
-  el.textContent = 'Что сегодня отпускаем?';
+  el.textContent = 'Что будем отпускать?';
   el.id = placeholderId;
   placeholderEl = el;
 
   return el;
+}
+
+function handleEmptyCard() {
+  topicCardBodyEl.insertAdjacentElement('afterbegin', createCardPlaceholder());
+  clearButtonEl.classList.add('d-none');
 }
 
 topicCardBodyEl.addEventListener('click', (event) => {
@@ -22,6 +28,18 @@ topicCardBodyEl.addEventListener('click', (event) => {
 
 topicCardBodyEl.addEventListener('blur', (event) => {
   if (!event.target.textContent.trim()) {
-    event.target.insertAdjacentElement('afterbegin', createCardPlaceholder());
+    handleEmptyCard();
   }
+});
+
+topicCardBodyEl.addEventListener('input', () => {
+  clearButtonEl.classList.remove('d-none');
+});
+
+clearButtonEl.addEventListener('click', (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+
+  topicCardBodyEl.removeChild(topicCardBodyEl.firstChild);
+  handleEmptyCard();
 });
