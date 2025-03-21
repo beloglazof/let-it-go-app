b9d12c64bf8f3eaddb4add54b33be8c7b479a51b
Slava <slavathedeveloper@gmail.com>
2025-03-21 13:06:21 +0300

    add settings

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { incCounter } from './src/counter/counter-controller';
 
 const catEl = document.getElementById('cat');
 const stoneFallSound = document.getElementById('stone-fall-sound');
-const soundStatusButton = document.getElementById('sound-status-button');
+const soundStatus = document.getElementById('sound-status');
 
 if (window.Telegram) {
   document.documentElement.dataset.bsTheme = window.Telegram.WebApp.colorScheme;
@@ -23,34 +23,16 @@ function letGo() {
     return;
   }
 
-  const withSound = soundStatusButton.dataset.status === 'on';
+  const withSound = soundStatus.checked;
   const sound = withSound ? stoneFallSound : null;
 
   animate(catEl, animationSpeedStore.speed, sound);
   incCounter();
 }
 
-document.addEventListener('click', letGo);
+catEl.addEventListener('click', letGo);
 document.addEventListener('keydown', (event) => {
   if (event.key === ' ') {
     letGo();
   }
 });
-
-soundStatusButton.addEventListener(
-  'click',
-  function (event) {
-    event.stopPropagation();
-    event.preventDefault();
-
-    if (this.dataset.status === 'on') {
-      this.dataset.status = 'off';
-      this.textContent = '🔈';
-    } else if (this.dataset.status === 'off') {
-      this.dataset.status = 'on';
-      this.textContent = '🔇';
-    }
-    this.setAttribute('aria-checked', state ? 'false' : 'true');
-  },
-  false,
-);
