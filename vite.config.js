f74b2cc891d68aff85810900a5b3753fc1303b7b
Slava <slavathedeveloper@gmail.com>
2025-07-01 19:03:28 +0300

    init PWA (v2)

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
-import { resolve } from 'path';
-import { preview } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default {
   server: {
@@ -8,6 +7,37 @@ export default {
   preview: {
     port: 3801,
   },
+  plugins: [
+    VitePWA({
+      manifest: {
+        name: 'Просто отпусти это',
+        short_name: 'Отпускай',
+        description: 'Вместе с котиком :3',
+        theme_color: '#ffffff',
+        background_color: '#2d2d2d',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'cat-frames/*.svg',
+      ],
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: { enabled: true },
+    }),
+  ],
   // Optional: Silence Sass deprecation warnings. See note below.
   css: {
     preprocessorOptions: {
