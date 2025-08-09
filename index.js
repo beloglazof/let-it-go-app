dbf27a63179594fc4e59ef774e75932291f62a84
Slava <slavathedeveloper@gmail.com>
2025-08-09 04:18:55 +0300

    add campfire

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import './src/topic-card/topic-card-controller';
 import './src/music/music-controller';
 import './src/animation/animation-speed-controller';
 import './styles.scss';
+import './src/campfire.scss'
 
 const catEl = document.getElementById('cat');
 const catContainerEl = document.getElementById('cat-container');
